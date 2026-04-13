7ee853fc178bccb6c97f3cefe4356411e91c894a
PDN237 <binbokhoa123@gmail.com>
2026-04-13 04:04:58 -0700

    update

--- routes/information.route.js
+++ routes/information.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const informationController = require('../controllers/Information');
 
 // Information API - Get instructor profile and courses
-router.get('/information/:id', informationController.getInstructorById);
+router.get('/:id', informationController.getInstructorById);
 
 module.exports = router;
